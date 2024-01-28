6edd693dc3bcf88bb5a919b481b5dc5780b89f4b
Splitu <bartosz.pekala1106@gmail.com>
2024-01-28 17:17:26 +0100

    Created form for AddScore class

---
+++ Projekt/src/AddScore.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class AddScore {
+    private JPanel JPanel;
+    private JButton backBtn;
+    private JButton exitBtn;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton sendBtn;
+}
